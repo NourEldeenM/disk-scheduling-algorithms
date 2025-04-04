d09fee9ebcc61af2cab690736f95ae6649ac1f1c
Nour Eldeen Mohamed <119622675+NOURisLIGHT@users.noreply.github.com>
2025-04-04 11:39:01 +0200

    feat(scheduler): add SSTF, SCAN disk scheduling algorithms

--- src/DiskScheduler.cpp
+++ src/DiskScheduler.cpp
@@ -3,6 +3,7 @@
 #include <ctime>
 #include <cstdlib>
 #include <cmath>
+#include <unordered_set>
 
 using namespace std;
 
@@ -29,7 +30,12 @@ public:
             requests.push_back(rand() % CYLINDERS_NUMBER);
         }
     }
-    
+
+    // time complexity: O(REQUESTS_NUMBER)
+    // space complexity: O(1)
+
+    /// @brief First-Come-First-Serve disk scheduling algorithm
+    /// @return total amount of head movement from start position
     int fcfs()
     {
         int currentPosition = initialHeadPosition, headMovement = 0;
@@ -41,8 +47,64 @@ public:
         return headMovement;
     }
 
-    // int sstf();
-    // int scan();
+    /// @brief Shortest-Seek-Time-First disk scheduling algorithm
+    /// @return total amount of head movement from start position
+    int sstf() {
+        int currentPosition = initialHeadPosition;
+        int headMovement = 0;
+        
+        unordered_set<int> servedRequests;
+        while (servedRequests.size() < REQUESTS_NUMBER) {
+            int minDistance = INT_MAX;
+            int servedRequestIndex;
+            for (int i = 0; i < REQUESTS_NUMBER; i++) {
+                if (servedRequests.find(i) == servedRequests.end()) { // if this request not served
+                    if (abs(requests[i] - currentPosition) < minDistance) { // check if its seek time is the least or not
+                        minDistance = abs(requests[i] - currentPosition);
+                        servedRequestIndex = i;
+                    }
+                }
+            }
+            headMovement += minDistance;
+            servedRequests.insert(servedRequestIndex);
+        }
+        return headMovement;
+    }
+
+    int scan() {
+        int currentPosition = initialHeadPosition;
+        int headMovement = 0;
+
+        vector<int> tempRequests = requests;
+        sort(tempRequests.begin(), tempRequests.end());
+
+        int startIdx = 0;
+        for (int i = 0; i < REQUESTS_NUMBER; i++)
+        {
+            if (tempRequests[i] >= currentPosition)
+            {
+                startIdx = i;
+                break;
+            }
+        }
+
+        // move to highest request
+        for (int i = startIdx; i < REQUESTS_NUMBER; i++) {
+            headMovement += abs(currentPosition - tempRequests[i]);
+            currentPosition = tempRequests[i];
+        }
+
+        headMovement += abs(currentPosition - (CYLINDERS_NUMBER - 1)); // simulate it reached the end
+        currentPosition = CYLINDERS_NUMBER - 1;
+
+        if (startIdx > 0) {
+            for (int i = startIdx - 1; i >= 0; i--) {
+                headMovement += abs(currentPosition - tempRequests[i]);
+                currentPosition = tempRequests[i];
+            }
+        }
+        return headMovement;
+    }
     // int cscan();
     // int look();
     // int clook();
--- src/main.cpp
+++ src/main.cpp
@@ -4,12 +4,10 @@
 using namespace std;
 
 int main(int argc, char *argv[]) {
-    if (argc != 2) {
-        cout << "Expected input: " << argv[0] << " <initial_head_position>\n";
-        return 1;
-    }
 
-    int initialPosition = atoi(argv[1]);
+    int initialPosition;
+    cout << "Enter start position: ";
+    cin >> initialPosition;
     if (initialPosition < 0 || initialPosition > 4999) {
         cout << "Invalid initial position, must be between 0 & 4999\n";
         return 1;
@@ -19,8 +17,8 @@ int main(int argc, char *argv[]) {
 
     cout << "Total head movement for each algorithm:" << endl;
     cout << "FCFS:\t" << scheduler.fcfs() << endl;
-    // cout << "SSTF:\t" << scheduler.sstf() << endl;
-    // cout << "SCAN:\t" << scheduler.scan() << endl;
+    cout << "SSTF:\t" << scheduler.sstf() << endl;
+    cout << "SCAN:\t" << scheduler.scan() << endl;
     // cout << "C-SCAN:\t" << scheduler.cscan() << endl;
     // cout << "LOOK:\t" << scheduler.look() << endl;
     // cout << "C-LOOK:\t" << scheduler.clook() << endl;
